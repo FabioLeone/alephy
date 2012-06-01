e1e560c31788e4270526fd29d29a4fe0debae8fc
fabiohleone <fabiohleone@e66ce4b0-e0cc-487d-a709-3c2a22bce054>
2012-06-01 01:45:02 +0000

    Implementado a função edit para as telas de cadastro de usuario, redes e loja.

--- trunk/RO/Implementação/fontes/RO/SIAO/wfmCadastroLojas.aspx.cs
+++ trunk/RO/Implementação/fontes/RO/SIAO/wfmCadastroLojas.aspx.cs
@@ -11,156 +11,260 @@ namespace SIAO
         string scn = ConfigurationManager.ConnectionStrings["SIAOConnectionString"].ConnectionString;
         SRV.clsControl o = new SRV.clsControl();
         SRV.clsFuncs of = new SRV.clsFuncs();
+        SRV.Loja clsLoja = new SRV.Loja();
 
-       protected void Page_Load(object sender, EventArgs e)
+        protected void Page_Load(object sender, EventArgs e)
         {
             if (Session["user"] == null) { Response.Redirect("Logon.aspx"); }
 
-            if (!IsPostBack) {
+            if (Session["editL"] != null) { clsLoja.Id = (int)Session["editL"]; }
+
+            if (!IsPostBack)
+            {
                 LoadRedes();
                 LoadUf();
+                getLojas();
                 txtNomeFantasia.Focus();
             }
         }
 
-       private void LoadUf()
-       {
-           DataSet ds = new DataSet();
-           ds = o.GetUf(scn);
+        private void getLojas()
+        {
+            ddlLoja.DataSource = o.GetFarmacias(scn);
+            ddlLoja.DataTextField = "NomeFantasia";
+            ddlLoja.DataValueField = "Id";
+            ddlLoja.DataBind();
+            ddlLoja.Items.Insert(0, new ListItem(string.Empty, string.Empty));
+            ddlLoja.SelectedIndex = 0;
+        }
 
-           if (ds.Tables.Count > 0) {
-               ddlUf.DataSource = ds.Tables[0];
-               ddlUf.DataValueField = ds.Tables[0].Columns[0].ToString();
-               ddlUf.DataTextField = ds.Tables[0].Columns[1].ToString();
-               ddlUf.DataBind();
-               ddlUf.Items.Insert(0, new ListItem(String.Empty, String.Empty));
-               ddlUf.SelectedIndex = 0;
-           }
-       }
+        private void LoadUf()
+        {
+            DataSet ds = new DataSet();
+            ds = o.GetUf(scn);
 
-       private void LoadRedes()
-       {
-           DataSet ds = new DataSet();
-           ds = o.GetRedes(scn);
+            if (ds.Tables.Count > 0)
+            {
+                ddlUf.DataSource = ds.Tables[0];
+                ddlUf.DataValueField = ds.Tables[0].Columns[0].ToString();
+                ddlUf.DataTextField = ds.Tables[0].Columns[1].ToString();
+                ddlUf.DataBind();
+                ddlUf.Items.Insert(0, new ListItem(String.Empty, String.Empty));
+                ddlUf.SelectedIndex = 0;
+            }
+        }
 
-           if (ds.Tables.Count > 0) {
-               ddlRede.DataSource = ds.Tables[0];
-               ddlRede.DataTextField = ds.Tables[0].Columns[1].ToString();
-               ddlRede.DataValueField = ds.Tables[0].Columns[0].ToString();
-               ddlRede.DataBind();
-               ddlRede.Items.Insert(0, new ListItem(String.Empty, String.Empty));
-               ddlRede.SelectedIndex = 0;
-           }
-       }
+        private void LoadRedes()
+        {
+            DataSet ds = new DataSet();
+            ds = o.GetRedes(scn);
 
-       private void divErro(string msg)
-       {
-           System.Web.UI.HtmlControls.HtmlGenericControl divError = new System.Web.UI.HtmlControls.HtmlGenericControl("div");
+            if (ds.Tables.Count > 0)
+            {
+                ddlRede.DataSource = ds.Tables[0];
+                ddlRede.DataTextField = ds.Tables[0].Columns[1].ToString();
+                ddlRede.DataValueField = ds.Tables[0].Columns[0].ToString();
+                ddlRede.DataBind();
+                ddlRede.Items.Insert(0, new ListItem(String.Empty, String.Empty));
+                ddlRede.SelectedIndex = 0;
+            }
+        }
 
-           divError.ID = "msgError";
-           divError.Attributes.Add("class", "error");
-           divError.Style.Add(HtmlTextWriterStyle.MarginLeft, "4%");
-           divError.InnerHtml = "<p>" + msg + "</p>";
+        private void divErro(string msg)
+        {
+            System.Web.UI.HtmlControls.HtmlGenericControl divError = new System.Web.UI.HtmlControls.HtmlGenericControl("div");
 
-           UpdatePanel1.ContentTemplateContainer.Controls.Add(divError);
-       }
+            divError.ID = "msgError";
+            divError.Attributes.Add("class", "error");
+            divError.Style.Add(HtmlTextWriterStyle.MarginLeft, "4%");
+            divError.InnerHtml = "<p>" + msg + "</p>";
 
-       private void divSces()
-       {
-           System.Web.UI.HtmlControls.HtmlGenericControl divInfo = new System.Web.UI.HtmlControls.HtmlGenericControl("div");
+            UpdatePanel1.ContentTemplateContainer.Controls.Add(divError);
+        }
 
-           divInfo.ID = "msgInfo";
-           divInfo.Attributes.Add("class", "success");
-           divInfo.Style.Add(HtmlTextWriterStyle.MarginLeft, "32%");
-           divInfo.InnerHtml = "<p>Loja cadastrada com sucesso.</p>";
+        private void divSces(string msg)
+        {
+            System.Web.UI.HtmlControls.HtmlGenericControl divInfo = new System.Web.UI.HtmlControls.HtmlGenericControl("div");
 
-           UpdatePanel1.ContentTemplateContainer.Controls.Add(divInfo);
-       }
+            divInfo.ID = "msgInfo";
+            divInfo.Attributes.Add("class", "success");
+            divInfo.Style.Add(HtmlTextWriterStyle.MarginLeft, "32%");
+            divInfo.InnerHtml = "<p>" + msg + "</p>";
 
-       protected void btnSave_Click(object sender, EventArgs e)
-       {
-           if (txtCidade.Text == "") { divErro("Entre com a cidade."); txtCidade.Focus(); }
-           else if (!of.ValidaCnpj(txtCnpj.Text))
-           {
-               divErro("CNPJ invalido.");
-               txtCnpj.Focus();
-           }
-           else if (!of.isEmail(txtEmail.Text)) { divErro("Email inválido."); txtEmail.Focus(); }
-           else if (txtFone.Text == "")
-           {
-               divErro("Entre com o número do telefone.");
-               txtFone.Focus();
-           }
-           else if (txtGerente.Text == "") { divErro("Entre com o nome do Gerente da Loja."); txtGerente.Focus(); }
-           else if (txtNomeFantasia.Text == "")
-           {
-               divErro("Entre com o nome fantasia.");
-               txtNomeFantasia.Focus();
-           }
-           else if (txtProprietario.Text == "") { divErro("Entre com o nome do proprietário."); txtProprietario.Focus(); }
-           else if (txtRazao.Text == "")
-           {
-               divErro("Entre com a razão social.");
-               txtRazao.Focus();
-           }
-           else {
-               SRV.Loja ol = new SRV.Loja() { 
-                Ativo = cbxAtivo.Checked,
-                Bairro = txtBairro.Text,
-                Celular = txtCel.Text,
-                Cidade = txtCidade.Text,
-                Cnpj = txtCnpj.Text,
-                Complemento = txtComp.Text,
-                Email = txtEmail.Text,
-                Email2 = txtEmail2.Text,
-                Endereco = txtEndereco.Text,
-                EndNumero = Convert.ToInt32(txtNum.Text),
-                Fone = txtFone.Text,
-                Fone2 = txtFone2.Text,
-                Gerente = txtGerente.Text,
-                idRede = Convert.ToInt32(ddlRede.SelectedValue),
-                Msn = txtMsn.Text,
-                NomeFantasia = txtNomeFantasia.Text,
-                Proprietario = txtProprietario.Text,
-                Razao = txtRazao.Text,
-                Rede = ddlRede.SelectedItem.Text,
-                Site = txtSite.Text,
-                Skype = txtSkype.Text,
-                Uf = ddlUf.SelectedValue
-               };
+            UpdatePanel1.ContentTemplateContainer.Controls.Add(divInfo);
+        }
 
-               string msg = o.AddLoja(scn, ol);
+        protected void btnSave_Click(object sender, EventArgs e)
+        {
+            if (txtCidade.Text == "") { divErro("Entre com a cidade."); txtCidade.Focus(); }
+            else if (!of.ValidaCnpj(txtCnpj.Text))
+            {
+                divErro("CNPJ invalido.");
+                txtCnpj.Focus();
+            }
+            else if (!of.isEmail(txtEmail.Text)) { divErro("Email inválido."); txtEmail.Focus(); }
+            else if (txtFone.Text == "")
+            {
+                divErro("Entre com o número do telefone.");
+                txtFone.Focus();
+            }
+            else if (txtGerente.Text == "") { divErro("Entre com o nome do Gerente da Loja."); txtGerente.Focus(); }
+            else if (txtNomeFantasia.Text == "")
+            {
+                divErro("Entre com o nome fantasia.");
+                txtNomeFantasia.Focus();
+            }
+            else if (txtProprietario.Text == "") { divErro("Entre com o nome do proprietário."); txtProprietario.Focus(); }
+            else if (txtRazao.Text == "")
+            {
+                divErro("Entre com a razão social.");
+                txtRazao.Focus();
+            }
+            else
+            {
+                string msg = "";
 
-               if (msg != "") { divErro(msg); } else { divSces(); Clear(); }
-               
-           }
-       }
+                bool ed = false;
 
-       private void Clear()
-       {
-           cbxAtivo.Checked = false;
-           ddlRede.SelectedIndex = 0;
-           ddlUf.SelectedIndex = 0;
-           txtBairro.Text = "";
-           txtCel.Text = "";
-           txtCidade.Text = "";
-           txtCnpj.Text = "";
-           txtComp.Text = "";
-           txtEmail.Text = "";
-           txtEmail2.Text = "";
-           txtEndereco.Text = "";
-           txtFone.Text = "";
-           txtFone2.Text = "";
-           txtGerente.Text = "";
-           txtMsn.Text = "";
-           txtNomeFantasia.Text = "";
-           txtNum.Text = "";
-           txtProprietario.Text = "";
-           txtRazao.Text = "";
-           txtSite.Text = "";
-           txtSkype.Text = "";
+                if (clsLoja.Id > 0)
+                {
+                    clsLoja.Ativo = cbxAtivo.Checked;
+                    clsLoja.Bairro = txtBairro.Text;
+                    clsLoja.Celular = txtCel.Text;
+                    clsLoja.Cidade = txtCidade.Text;
+                    clsLoja.Cnpj = txtCnpj.Text;
+                    clsLoja.Complemento = txtComp.Text;
+                    clsLoja.Email = txtEmail.Text;
+                    clsLoja.Email2 = txtEmail2.Text;
+                    clsLoja.Endereco = txtEndereco.Text;
+                    clsLoja.EndNumero = Convert.ToInt32(txtNum.Text);
+                    clsLoja.Fone = txtFone.Text;
+                    clsLoja.Fone2 = txtFone2.Text;
+                    clsLoja.Gerente = txtGerente.Text;
+                    clsLoja.idRede = Convert.ToInt32(ddlRede.SelectedValue);
+                    clsLoja.Msn = txtMsn.Text;
+                    clsLoja.NomeFantasia = txtNomeFantasia.Text;
+                    clsLoja.Proprietario = txtProprietario.Text;
+                    clsLoja.Razao = txtRazao.Text;
+                    clsLoja.Rede = ddlRede.SelectedItem.Text;
+                    clsLoja.Site = txtSite.Text;
+                    clsLoja.Skype = txtSkype.Text;
+                    clsLoja.Uf = ddlUf.SelectedValue;
 
-           txtNomeFantasia.Focus();
-       }
+                    msg = o.UpdateLoja(scn, clsLoja);
+                    ed = true;
+                }
+                else
+                {
+                    clsLoja = new SRV.Loja()
+                    {
+                        Ativo = cbxAtivo.Checked,
+                        Bairro = txtBairro.Text,
+                        Celular = txtCel.Text,
+                        Cidade = txtCidade.Text,
+                        Cnpj = txtCnpj.Text,
+                        Complemento = txtComp.Text,
+                        Email = txtEmail.Text,
+                        Email2 = txtEmail2.Text,
+                        Endereco = txtEndereco.Text,
+                        EndNumero = Convert.ToInt32(txtNum.Text),
+                        Fone = txtFone.Text,
+                        Fone2 = txtFone2.Text,
+                        Gerente = txtGerente.Text,
+                        idRede = Convert.ToInt32(ddlRede.SelectedValue),
+                        Msn = txtMsn.Text,
+                        NomeFantasia = txtNomeFantasia.Text,
+                        Proprietario = txtProprietario.Text,
+                        Razao = txtRazao.Text,
+                        Rede = ddlRede.SelectedItem.Text,
+                        Site = txtSite.Text,
+                        Skype = txtSkype.Text,
+                        Uf = ddlUf.SelectedValue
+                    };
+
+                    msg = o.AddLoja(scn, clsLoja);
+                }
+
+                if (msg != "") { divErro(msg); }
+                else
+                {
+                    if (ed)
+                    {
+                        divSces("Alterações salvas com sucesso.");
+                        Clear();
+                    }
+                    else { divSces("Loja cadastrada com sucesso."); Clear(); }
+                }
+
+            }
+        }
+
+        private void Clear()
+        {
+            cbxAtivo.Checked = false;
+            ddlRede.SelectedIndex = 0;
+            ddlUf.SelectedIndex = 0;
+            txtBairro.Text = "";
+            txtCel.Text = "";
+            txtCidade.Text = "";
+            txtCnpj.Text = "";
+            txtComp.Text = "";
+            txtEmail.Text = "";
+            txtEmail2.Text = "";
+            txtEndereco.Text = "";
+            txtFone.Text = "";
+            txtFone2.Text = "";
+            txtGerente.Text = "";
+            txtMsn.Text = "";
+            txtNomeFantasia.Text = "";
+            txtNum.Text = "";
+            txtProprietario.Text = "";
+            txtRazao.Text = "";
+            txtSite.Text = "";
+            txtSkype.Text = "";
+
+            Session["editL"] = null;
+
+            txtNomeFantasia.Focus();
+        }
+
+        protected void btnEdit_Click(object sender, EventArgs e)
+        {
+            SRV.Loja ol = new SRV.Loja();
+            ol = o.GetLojaEdit(scn, ddlLoja.SelectedValue);
+
+            if (ol.Id > 0)
+            {
+                try
+                {
+                    ddlRede.SelectedValue = ol.idRede.ToString();
+                    ddlUf.SelectedValue = ol.Uf;
+                }
+                catch
+                {
+                }
+
+                txtBairro.Text = ol.Bairro;
+                txtCel.Text = ol.Celular;
+                txtCidade.Text = ol.Cidade;
+                txtCnpj.Text = ol.Cnpj;
+                txtComp.Text = ol.Complemento;
+                txtEmail.Text = ol.Email;
+                txtEmail2.Text = ol.Email2;
+                txtEndereco.Text = ol.Endereco;
+                txtFone.Text = ol.Fone;
+                txtFone2.Text = ol.Fone2;
+                txtGerente.Text = ol.Gerente;
+                txtMsn.Text = ol.Msn;
+                txtNomeFantasia.Text = ol.NomeFantasia;
+                txtNum.Text = ol.EndNumero.ToString();
+                txtProprietario.Text = ol.Proprietario;
+                txtRazao.Text = ol.Razao;
+                txtSite.Text = ol.Site;
+                txtSkype.Text = ol.Skype;
+
+                Session["editL"] = ol.Id;
+            }
+        }
     }
 }
--- trunk/RO/Implementação/fontes/RO/SIAO/wfmCadastroLojas.aspx.designer.cs
+++ trunk/RO/Implementação/fontes/RO/SIAO/wfmCadastroLojas.aspx.designer.cs
@@ -22,6 +22,24 @@ namespace SIAO {
         protected global::System.Web.UI.UpdatePanel UpdatePanel1;
         
         /// <summary>
+        /// ddlLoja control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList ddlLoja;
+        
+        /// <summary>
+        /// btnEdit control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnEdit;
+        
+        /// <summary>
         /// txtNomeFantasia control.
         /// </summary>
         /// <remarks>
