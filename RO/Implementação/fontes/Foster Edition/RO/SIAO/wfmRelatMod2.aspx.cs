d13bae6dd5fc5d2ca54978f1435ec6b28dbd4aed
ferfoster@gmail.com <ferfoster@gmail.com@e66ce4b0-e0cc-487d-a709-3c2a22bce054>
2012-07-26 15:22:51 +0000

    Alterado Grafico!

---
+++ RO/Implementação/fontes/Foster Edition/RO/SIAO/wfmRelatMod2.aspx.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using SIAO.SRV.TO;
+using System.Configuration;
+
+namespace SIAO
+{
+    public partial class wfmRelatMod2 : System.Web.UI.Page
+    {
+        List<SRV.clsRelat1> lr = new List<SRV.clsRelat1>();
+        UsersTO clsUser = new UsersTO();
+        SRV.clsControl oc = new SRV.clsControl();
+        string scn = ConfigurationManager.ConnectionStrings["SIAOConnectionString"].ConnectionString;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["user"] == null) { Response.Redirect("Logon.aspx"); }
+            else
+            {
+                clsUser = (UsersTO)Session["user"];
+            }
+
+            if (!IsPostBack)
+            {
+                lr = (List<SRV.clsRelat1>)Session["cross"];
+                loadRelat(lr, clsUser);
+            }
+        }
+
+        private void loadRelat(List<SRV.clsRelat1> lr, UsersTO clsUser)
+        {
+            if (lr.Count == 0) { lr = oc.GetCross(scn, clsUser, ""); }
+
+            Microsoft.Reporting.WebForms.ReportDataSource Rds = new Microsoft.Reporting.WebForms.ReportDataSource("DataSet1", lr);
+            ReportViewer1.Reset();
+            ReportViewer1.LocalReport.Dispose();
+            ReportViewer1.LocalReport.DataSources.Add(Rds);
+            ReportViewer1.LocalReport.ReportPath = "Relatory/rptCross2.rdlc";
+            ReportViewer1.DataBind();
+        }
+    }
+}
