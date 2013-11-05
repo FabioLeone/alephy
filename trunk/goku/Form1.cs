d5692af408d102d07e21b7af3751bd0e7576b400
fabiohleone <fabiohleone@e66ce4b0-e0cc-487d-a709-3c2a22bce054>
2013-11-05 16:10:37 +0000

    app de envio automatico de arquivos

---
+++ trunk/goku/Form1.cs
@@ -0,0 +1,59 @@
+﻿using consolidate.resources;
+using goku.resources;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace goku
+{
+    public partial class Form1 : Form
+    {
+        monitor m;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            getIni();
+
+            contention.cleanFiles();
+
+            m = new monitor();
+            this.Hide();
+        }
+
+        private void getIni()
+        {
+            string s = AppDomain.CurrentDomain.SetupInformation.ApplicationBase + "iniRO.ini";
+
+            if (File.Exists(s))
+            {
+                monitor.fileConfig(iniFile.ReadValue(s, "security", "mail"));
+            }
+            else {
+                Confg c = new Confg();
+                c.Show();
+            }
+        }
+
+        private void tSMenuItem1_Click(object sender, EventArgs e)
+        {
+            Confg c = new Confg();
+            c.Show();
+        }
+
+        private void tSMenuItem3_Click(object sender, EventArgs e)
+        {
+            m.t.Abort();
+            this.Close();
+        }
+
+    }
+}
---
+++ trunk/goku/resources/monitor.cs
@@ -0,0 +1,391 @@
+﻿using goku.resources;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace consolidate.resources
+{
+    public class monitor
+    {
+        private List<int> lstMeses = new List<int>();
+        private List<string> lstCnpj = new List<string>();
+        private string strPath = AppDomain.CurrentDomain.SetupInformation.ApplicationBase + "files\\";
+        public Thread t;
+
+        public monitor()
+        {
+            t = new Thread(getFiles);
+            t.IsBackground = true;
+            t.Start();
+
+        }
+
+        private void getFiles()
+        {
+            string r;
+            string p;
+
+            while (true)
+            {
+                r = string.Empty;
+                p = string.Empty;
+                try
+                {
+                    if (Directory.GetFiles(strPath).Count() > 0)
+                    {
+                        p = Directory.GetFiles(strPath).First();
+                        r = fileProcess(p);
+
+                        if (!String.IsNullOrEmpty(r.Replace("\r\n", "")))
+                            sendErrorEmail(r, p);
+                        else
+                            sendEmail(r, p);
+                    }
+                    else
+                        Thread.Sleep(3000);
+                }
+                catch (IOException)
+                {
+                    Thread.Sleep(3000);
+                }
+            }
+        }
+
+        private void sendErrorEmail(string r, string p)
+        {
+            MailMessage message = new MailMessage();
+            message.From = new MailAddress("fabio@casbrasil.com.br");
+            message.To.Add("fabio@casbrasil.com.br");
+            message.To.Add(ConfigurationManager.AppSettings["EMAIL_SEND"]);
+
+            message.Subject = "Erro na carga arquivo.";
+
+            StringBuilder sbMsg = new StringBuilder();
+            sbMsg.Append(string.Format("Arquivo {0} contém erros {1}", System.IO.Path.GetFileName(p), Environment.NewLine));
+            sbMsg.Append(string.Format("Data: {0}{1}", DateTime.Now.ToString("dd/MM/yyyy hh:mm"), Environment.NewLine));
+            sbMsg.Append(string.Format("Erros: {0}{1}", Environment.NewLine, r));
+
+            message.Body = sbMsg.ToString();
+
+            SmtpClient client = new SmtpClient();
+
+            client.Host = "smtp.casbrasil.com.br";
+            client.Port = 587;
+            client.Credentials = new System.Net.NetworkCredential("fabio@casbrasil.com.br", "ale0112358");
+
+            try
+            {
+                client.Send(message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception caught in sendEmail(): {0}",
+                      ex.ToString());
+            }
+            finally
+            {
+                client.Dispose();
+            }
+        }
+
+        private void sendEmail(string r, string p)
+        {
+            MailMessage message = new MailMessage();
+            message.From = new MailAddress("fabio@casbrasil.com.br");
+            message.To.Add(ConfigurationManager.AppSettings["EMAIL_SEND"]);
+
+            message.Subject = "Arquivo enviado com sucesso.";
+
+            StringBuilder sbMsg = new StringBuilder();
+            sbMsg.Append(string.Format("Arquivo {0} foi enviado com sucesso {1}", System.IO.Path.GetFileName(p), Environment.NewLine));
+            sbMsg.Append(string.Format("Data: {0}{1}", DateTime.Now.ToString("dd/MM/yyyy hh:mm"), Environment.NewLine));
+
+            message.Body = sbMsg.ToString();
+
+            SmtpClient client = new SmtpClient();
+
+            client.Host = "smtp.casbrasil.com.br";
+            client.Port = 587;
+            client.Credentials = new System.Net.NetworkCredential("fabio@casbrasil.com.br", "ale0112358");
+
+            try
+            {
+                client.Send(message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception caught in sendEmail(): {0}",
+                      ex.ToString());
+            }
+            finally
+            {
+                client.Dispose();
+            }
+        }
+
+        private string fileProcess(string strP)
+        {
+            StringBuilder msg = new StringBuilder();
+            List<string> lst = new List<string>();
+
+            if (validaExt(strP))
+            {
+                if (System.IO.Path.GetExtension(strP).ToUpper() == ".XML")
+                {
+                    XmlDocument xd = new XmlDocument();
+
+                    try
+                    {
+                        xd.Load(strP);
+
+                        DataTable dt = ConvertXML(xd);
+
+                        msg.Append(cnpjValidation(dt));
+                        lst = charValidation(dt);
+                        if (lst.Count > 0)
+                            msg.Append(string.Format("{0}O arquivo contém os seguintes caracteres especiais: {1}", Environment.NewLine, lst.Aggregate((i, j) => i + Environment.NewLine + j)));
+                        else
+                            msg.Append(string.Format("{0}{1}", Environment.NewLine, dal.insertXml(dt)));
+                    }
+                    catch (Exception e)
+                    {
+                        msg.Append(e.Message);
+                    }
+
+                }
+                else
+                {
+                    DataTable dt = new DataTable();
+                    dt = txtDtConvert(strP);
+
+                    lst = charValidation(dt);
+                    if (lst.Count > 0)
+                        msg.Append(string.Format("O arquivo contém os seguintes caracteres especiais: {0}", lst.Aggregate((i, j) => i + " " + j)));
+                    else
+                        msg.Append(dal.inserTxt(dt));
+                }
+            }
+
+            if (string.IsNullOrEmpty(msg.ToString().Replace("\r\n", "")))
+            {
+                fileMove(strP);
+                consolidation c = new consolidation();
+            }
+            else
+                fileErrorMove(strP);
+
+            return msg.ToString();
+        }
+
+        private void fileErrorMove(string strP)
+        {
+            string s = string.Empty;
+
+            if (File.Exists(strP))
+            {
+                s = AppDomain.CurrentDomain.SetupInformation.ApplicationBase + "errorfiles";
+                if (!Directory.Exists(s))
+                    Directory.CreateDirectory(s);
+
+                s = s + "\\" + System.IO.Path.GetFileName(strP);
+
+                File.Copy(strP, s, true);
+
+                if (File.Exists(s))
+                    File.Delete(strP);
+            }
+        }
+
+        private void fileMove(string strP)
+        {
+            string s = string.Empty;
+
+            if (File.Exists(strP))
+            {
+                s = AppDomain.CurrentDomain.SetupInformation.ApplicationBase + "sentfiles";
+                if (!Directory.Exists(s))
+                    Directory.CreateDirectory(s);
+
+                s = s + "\\" + System.IO.Path.GetFileName(strP);
+
+                File.Copy(strP, s, true);
+
+                if (File.Exists(s))
+                    File.Delete(strP);
+            }
+        }
+
+        private DataTable txtDtConvert(string stream)
+        {
+            DataTable dt = new DataTable();
+            StreamReader sr = new StreamReader(stream, Encoding.GetEncoding("Windows-1252"), true);
+            string[] line;
+            int i = 0;
+            int intMes = 0;
+            string strCnpj = string.Empty;
+
+            while (!sr.EndOfStream)
+            {
+                line = sr.ReadLine().Split(';');
+
+                if (i == 0)
+                {
+                    for (int j = 0; j < line.Length; j++)
+                    {
+                        dt.Columns.Add(RemoveSpecialChar(line[j].Trim()));
+                    }
+                    i++;
+                }
+                else
+                {
+                    DataRow dr = dt.NewRow();
+
+                    for (int j = 0; j < dt.Columns.Count; j++)
+                    {
+                        if (j.Equals(2))
+                        {
+                            if (!intMes.Equals(Convert.ToInt32(line[j])))
+                            {
+                                intMes = Convert.ToInt32(line[j]);
+                                lstMeses.Add(Convert.ToInt32(line[j]));
+                            }
+                        }
+                        if (j.Equals(1))
+                        {
+                            line[j] = RemoveMaskCnpj(line[j]);
+
+                            if (!strCnpj.Equals(line[j]))
+                            {
+                                strCnpj = line[j];
+                                lstCnpj.Add(strCnpj);
+                            }
+                        }
+                        dr[j] = line[j].Trim();
+                    }
+                    dt.Rows.Add(dr);
+                }
+            }
+
+            return dt;
+        }
+
+        public static string RemoveSpecialChar(string strText)
+        {
+            string normalized = strText.Normalize(NormalizationForm.FormD);
+
+            StringBuilder resultBuilder = new StringBuilder();
+            foreach (var character in normalized)
+            {
+                UnicodeCategory category = CharUnicodeInfo.GetUnicodeCategory(character);
+                if (category == UnicodeCategory.LowercaseLetter
+                    || category == UnicodeCategory.UppercaseLetter
+                    || category == UnicodeCategory.SpaceSeparator)
+                    resultBuilder.Append(character);
+            }
+
+            return Regex.Replace(resultBuilder.ToString(), @"\s+", "");
+        }
+
+        public static string RemoveMaskCnpj(string p)
+        {
+            string cnpj = "";
+            if (string.IsNullOrEmpty(p))
+                return cnpj;
+            else
+            {
+                cnpj = p.Replace(".", "");
+                cnpj = cnpj.Replace("/", "");
+                cnpj = cnpj.Replace("-", "");
+
+                return cnpj;
+            }
+        }
+
+        private List<string> charValidation(DataTable dt)
+        {
+            Regex r = new Regex(@"(?:[^a-z0-9 _\-\.\,\()\/\%\+\*\$\:\=\@\\]|(?<=['""])s)", RegexOptions.IgnoreCase | RegexOptions.CultureInvariant | RegexOptions.Compiled);
+
+            List<string> lstr = new List<string>();
+
+            foreach (DataRow row in dt.Rows)
+            {
+                foreach (var d in row.ItemArray)
+                {
+                    if (r.IsMatch(d.ToString()))
+                        if (lstr.FindAll(i => i == r.Match(d.ToString()).ToString()).Count.Equals(0)) lstr.Add(r.Match(d.ToString()).ToString());
+                }
+            }
+
+            return lstr;
+        }
+
+        public bool validaExt(string strP)
+        {
+            if (System.IO.Path.GetExtension(strP).ToUpper() == ".XML" || System.IO.Path.GetExtension(strP).ToUpper() == ".TXT") { return true; } else { return false; }
+        }
+
+        private static DataTable ConvertXML(XmlDocument xd)
+        {
+            DataSet ds = new DataSet();
+            int i = 0;
+
+            ds.ReadXml(new XmlNodeReader(xd));
+
+            ds.Tables[0].Columns["cnpj"].SetOrdinal(0);
+            ds.Tables[0].Columns["ean"].SetOrdinal(1);
+            ds.Tables[0].Columns["nprod"].SetOrdinal(2);
+            if (ds.Tables[0].Columns.Contains("grupo")) { ds.Tables[0].Columns["grupo"].SetOrdinal(3); i++; }
+            ds.Tables[0].Columns["fab"].SetOrdinal(3 + i);
+            ds.Tables[0].Columns["ano"].SetOrdinal(4 + i);
+            ds.Tables[0].Columns["mes"].SetOrdinal(5 + i);
+            ds.Tables[0].Columns["quant"].SetOrdinal(6 + i);
+            ds.Tables[0].Columns["vbruto"].SetOrdinal(7 + i);
+            ds.Tables[0].Columns["vliquido"].SetOrdinal(8 + i);
+            ds.Tables[0].Columns["desconto"].SetOrdinal(9 + i);
+
+            return ds.Tables[0];
+        }
+
+        private static string cnpjValidation(DataTable dt)
+        {
+            DataTable auxDt = new DataTable();
+            List<cvretun> lst = new List<cvretun>();
+            cvretun r;
+
+            auxDt = dt.DefaultView.ToTable(true, "cnpj");
+            if (auxDt.Rows.Count > 0)
+            {
+                for (int i = 0; i < auxDt.Rows.Count; i++)
+                {
+                    r = new cvretun() { cnpj = auxDt.Rows[i][0].ToString(), isValid = false };
+                    if (dal.validationByCNPJ(auxDt.Rows[i][0].ToString()) > 0) r.isValid = true;
+
+                    lst.Add(r);
+                }
+            }
+
+            if (lst.FindAll(s => s.isValid == false).Count > 0)
+                return string.Format("CNPJ(s) não cadastrado(s): {0}{1}", Environment.NewLine, lst.FindAll(s => s.isValid == false).Select(v => v.cnpj).Aggregate((t, u) => t + ";" + Environment.NewLine + u));
+            else
+                return string.Empty;
+        }
+
+        internal static void fileConfig(string p)
+        {
+            Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            config.AppSettings.Settings["EMAIL_SEND"].Value = p;
+            config.Save(ConfigurationSaveMode.Modified);
+            ConfigurationManager.RefreshSection("appSettings");
+        }
+    }
+}
